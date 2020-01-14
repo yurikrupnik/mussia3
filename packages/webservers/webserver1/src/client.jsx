d71c62a5021748247c10a3fe088087f808a49e3a
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-14 19:50:01 +0200

    webserver purple main color

--- packages/webservers/webserver1/src/client.jsx
+++ packages/webservers/webserver1/src/client.jsx
@@ -10,7 +10,7 @@ import './styles/_index.scss';
 const theme = {
     palette: {
         primary: {
-            main: '#5adc75'
+            main: '#a161dc'
         },
         shit: {
             main: '#b43fdc'
