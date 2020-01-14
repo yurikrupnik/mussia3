3241f76bf653e72364e49d46e634200ca88fc2da
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-14 19:57:12 +0200

    webserver red main color

--- packages/webservers/webserver1/src/client.jsx
+++ packages/webservers/webserver1/src/client.jsx
@@ -10,7 +10,7 @@ import './styles/_index.scss';
 const theme = {
     palette: {
         primary: {
-            main: '#a161dc'
+            main: '#dc5f4a'
         },
         shit: {
             main: '#b43fdc'
