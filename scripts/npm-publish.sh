4d03611c28c52938ede26ac2546a780e5be8dbee
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-15 01:52:10 +0200

    fixe amend

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -20,5 +20,5 @@ npm version patch
 #git push origin --follow-tags
 ###  # tag
   git status
-git tag "$(node -p "require('./package.json').version")" -a -m "taf for $(node -p "require('./package.json').version")"
+git tag -a "$(node -p "require('./package.json').version")" -m "taf for $(node -p "require('./package.json').version")"
 git push origin --follow-tags
