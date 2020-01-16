70ae65fca897150004ddf297a5978b307f8c334e
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-16 21:18:28 +0200

    fixed test

--- packages/ui/screen1/src/__tests__/screen1.jsx
+++ packages/ui/screen1/src/__tests__/screen1.jsx
@@ -1,5 +1,6 @@
 import React from 'react'; // eslint-disable-line
 import { render, cleanup } from '@testing-library/react';
+import { StaticRouter } from 'react-router-dom';
 import Component from '../index';
 
 afterEach(cleanup);
@@ -7,12 +8,14 @@ afterEach(cleanup);
 test(`render ${Component.name} Component`, () => {
     // expect(1).toBe(1);
     render((
-        <Component data={[
-            {
-                title: 'title 1',
-                context: 'Context stam'
-            }
-        ]}
-        />
+        <StaticRouter location="/">
+            <Component data={[
+                {
+                    title: 'title 1',
+                    context: 'Context stam'
+                }
+            ]}
+            />
+        </StaticRouter>
     ));
 });
