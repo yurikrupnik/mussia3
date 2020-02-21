e6b782ab20ff46d5fac3eae6af16d05a5bf5b385
yurikrupnik <krupnik.yuri@gmail.com>
2020-02-21 02:54:49 +0200

    st

--- packages/functions/func1/src/__tests__/func1.system.js
+++ packages/functions/func1/src/__tests__/func1.system.js
@@ -1,16 +1,15 @@
-// const Supertest = require('supertest');
+const Supertest = require('supertest');
 
-// const supertest = Supertest('www.google.com' || 'https://europe-west1-mussia3.cloudfunctions.net/func1');
+const supertest = Supertest(process.env.BASE_URL || 'https://europe-west1-mussia3.cloudfunctions.net/func1');
 
 describe('system tests', () => {
     it('func1 should return something', async () => {
-        expect(1).toEqual(1);
-        // await supertest
-        //     .post('/func1')
-        //     .send({ name: 'John' })
-        //     .expect(200)
-        //     .expect((response) => {
-        //         expect(response.body).toEqual({ all: 'aa' });
-        //     });
+        await supertest
+            .post('/func1')
+            .send({ name: 'John' })
+            .expect(200)
+            .expect((response) => {
+                expect(response.body).toEqual({ all: 'aa' });
+            });
     });
 });
