900228a65fdee3f577950b6a73ca72eee2aaab74
yurikrupnik <krupnik.yuri@gmail.com>
2020-08-01 11:49:24 +0300

    sa

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         }],
         'comma-dangle': 0,
         'no-underscore-dangle': 1,
-        'react/jsx-indent': [2, 4], // personal
+        'react/jsx-indent': [2, 4], // persona
         'react/jsx-indent-props': 0, // personal
         'jsx-a11y/anchor-is-valid': ['error', {
             'components': ['Link'],
---
+++ packages/models/users/__tests__/users.test.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+// const users = require('..');
+import fs from '../src/index';
+
+describe('users', () => {
+    it('needs tests', () => {
+        // const user = new I
+        fs();
+        expect(1).toBe(1);
+    });
+});
